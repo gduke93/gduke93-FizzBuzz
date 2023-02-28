2941a0c6cdf4e9a80056305e0004b00b24d92076
gduke93 <grayduke93@gmail.com>
2023-02-28 21:37:16 +0000

    Completed Challenge FizzBuzz

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,16 @@ public class Lab {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
+        if (n % 15 == 0){
+            return "FizzBuzz";
+        }
+        if (n % 3 == 0){
+            return "Fizz";
+        }
+        if (n % 5 == 0){
+            return "Buzz";
+        }
+
         return "";
     }
 }
